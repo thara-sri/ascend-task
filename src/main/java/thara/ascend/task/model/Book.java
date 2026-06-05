b2df089a4ee0be467c3a25a457d63eb44629929d
thara-sri <thara.sritharadol@gmail.com>
2026-06-05 14:52:29 +0700

    create book model

---
+++ src/main/java/thara/ascend/task/model/Book.java
@@ -0,0 +1,33 @@
+package thara.ascend.task.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "book", indexes = {
+        @Index(name = "idx_book_author", columnList = "author")
+})
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "title is needed")
+    @Column(nullable = false)
+    private String title;
+
+    @NotBlank(message = "author is needed")
+    @Column(nullable = false)
+    private String author;
+
+    @Column(name = "publishedDate")
+    private LocalDate publishedDate;
+
+}
+
+
